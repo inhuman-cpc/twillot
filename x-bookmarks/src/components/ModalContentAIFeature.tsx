f7c2a3ce8ab3b2932607f75c53b2d292e831ec11
SiZapPaaiGwat <simon@lionjs.com>
2025-01-11 18:11:45 +0800

    Update text

--- x-bookmarks/src/components/ModalContentAIFeature.tsx
+++ x-bookmarks/src/components/ModalContentAIFeature.tsx
@@ -5,7 +5,7 @@ export default function ModalContentAIFeature() {
       Twillot 2.0 is planned to release on January 30, 2025. At that time, all 1.0 users will be able to migrate to version 2.0 automatically for free.
       </p>
 
-      <p  class="flex items-center gap-2">While Twillot 1.0 only includes bookmark management features, Twillot 2.0 is much more powerful in functionality, capable of managing almost all Twitter personal data, including bookmarks/folders/likes/favorites/posts/replies/following/blocking and other modules. </p>
+      <p  class="flex items-center gap-2">While Twillot 1.0 only includes bookmark management features, Twillot 2.0 is much more powerful in functionality, capable of managing almost all Twitter personal data, including bookmarks / folders / likes / posts / replies / following / blocked and other modules. </p>
 
       <p class="flex items-center gap-2">Additionally, each module provides AI organization and folder functionality, while also supporting batch operations to improve efficiency.</p>
 
